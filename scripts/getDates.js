495050977a0f91e651092f9da016245f3c7bc946
Sakhile Mamba <sakhymamba@gmail.com>
2024-06-21 01:56:50 +0200

    responsive hamburger menu implementation

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,3 +2,10 @@ let date = new Date(Date.now()).getFullYear();
 document.getElementById("year").innerText = date;
 
 document.getElementById("lastModified").innerText = `Last modified: ${document.lastModified}`;
+
+const menu = document.querySelector('#menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('open');
+    document.querySelector('nav').classList.toggle('open');
+})
