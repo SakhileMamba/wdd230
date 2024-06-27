ff524ca303ff4f0e5f985bd051fc311d53c2681b
Sakhile Mamba <sakhymamba@gmail.com>
2024-06-27 20:03:38 +0200

    Responsive Images: srcset

--- week03/scripts/lazyload.js
+++
@@ -1,4 +0,0 @@
-let date = new Date(Date.now()).getFullYear();
-document.getElementById("year").innerText = date;
-
-document.getElementById("lastModified").innerText = `Last modified: ${document.lastModified}`;
