bcbc996bcd4507f41282409672df82e57293502f
Sakhile Mamba <sakhymamba@gmail.com>
2024-06-30 05:29:23 +0200

    W01: Project Milestone - The Chamber of Commerce Site Plan

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,4 @@
+let date = new Date(Date.now()).getFullYear();
+document.getElementById("year").innerText = date;
+
+document.getElementById("lastModified").innerText = `Last modified: ${document.lastModified}`;
