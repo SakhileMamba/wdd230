ec195a2186def44a696d352e6ac20ca454f306d2
Sakhile Mamba <sakhymamba@gmail.com>
2024-07-12 04:28:20 +0200

    json links start

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -23,4 +23,9 @@ function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
-    weatherIcon.setAttribut
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
