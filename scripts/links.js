d04a34d4b1ca018618d27496b7a9085313d18acd
Sakhile Mamba <sakhymamba@gmail.com>
2024-07-12 04:27:21 +0200

    json links start

---
+++ scripts/links.js
@@ -0,0 +1,12 @@
+const baseURL = "https://sakhilemamba.github.io/wdd230/";
+
+const linksURL = "https://sakhilemamba.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+}
+
+
+getLinks();
--- scripts/weather.js
+++ scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=c12475d7af51087fb11f15201dc9afcf"
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=-26.50&lon=31.31&units=metric&appid=c12475d7af51087fb11f15201dc9afcf"
 
 async function apiFetch() {
     try {
@@ -20,7 +20,12 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
-    weatherIcon.setAttribut
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
